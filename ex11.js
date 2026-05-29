241b9c25d486275eaf8754d6fd3b3599ec4c688e
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-28 17:33:35 -0700

    exercise 11 done

--- ex11.js
+++ ex11.js
@@ -1,9 +1,13 @@
 /*
-There's a new attraction at this year's Codeville festival. The organizers have decided to bring in several inflatable attractions, but they have no clue how to much blow them up. Each attraction needs to be pumped to a precise volume to achieve maximum festival fun!
+There's a new attraction at this year's Codeville festival. The organizers have decided to bring in several inflatable 
+attractions, but they have no clue how to much blow them up. Each attraction needs to be pumped to a precise volume 
+to achieve maximum festival fun!
 
-The attractions are each made up of a combination of several different shapes: cones, spheres and prisms. For example, the giant inflatable duck is made up of two spheres (the body and head) and a cone (the beak) 🦆.
+The attractions are each made up of a combination of several different shapes: cones, spheres and prisms. 
+For example, the giant inflatable duck is made up of two spheres (the body and head) and a cone (the beak) 🦆.
 
-Each shape has a different calculation for determining volume, so we'll need to create a few functions that will help us figure out the volume of the various inflatable attractions.
+Each shape has a different calculation for determining volume, so we'll need to create a few functions that will help us 
+figure out the volume of the various inflatable attractions.
 
 In this challenge, we'll need to implement four functions.
 
@@ -18,7 +22,8 @@ Don't worry about getting the answers to the perfect precision, just check to se
 Note
 We can find the formulas for calculating the volume of a sphere, cone, and prism online.
 
-The fourth function, totalVolume(), will receive an array containing the different shapes that make up a single attraction. The totalVolume function should use the previous three functions to calculate the total volume of an attraction.
+The fourth function, totalVolume(), will receive an array containing the different shapes that make up a single attraction. 
+The totalVolume function should use the previous three functions to calculate the total volume of an attraction.
 
 Instruction
 Implement the functions one by one. The example inputs and outputs below will help you check that each function is correct.
@@ -28,25 +33,46 @@ Implement the functions one by one. The example inputs and outputs below will he
 const PI = 3.14159;
 
 const sphereVolume = function (radius) {
-  // Code here!
+  return (4 / 3) * PI * radius * radius * radius;
 };
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); //true
 
 const coneVolume = function (radius, height) {
-  // And here!
+  return (1 / 3) * PI * radius * radius * height;
 };
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); //true
 
 const prismVolume = function (height, width, depth) {
-  // Probably here too!
+  return height * width * depth;
 };
 
 console.log(prismVolume(3, 4, 5) === 60); //true
 
 const totalVolume = function (solids) {
-  // Code here? Yup!
+  let volSphere = 0;
+  let volCone = 0;
+  let volPrism = 0;
+
+  solids.forEach((solid) => {
+    switch (solid.type) {
+      case "sphere":
+        volSphere = volSphere + sphereVolume(solid.radius);
+        break;
+
+      case "cone":
+        volCone = volCone + coneVolume(solid.radius, solid.height);
+        break;
+
+      case "prism":
+        volPrism =
+          volPrism + prismVolume(solid.height, solid.width, solid.depth);
+        break;
+    }
+  });
+
+  return volSphere + volPrism + volCone;
 };
 
 const largeSphere = {
@@ -66,6 +92,7 @@ const cone = {
 };
 
 const duck = [largeSphere, smallSphere, cone];
+totalVolume(duck);
 
 console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000); //true
 
