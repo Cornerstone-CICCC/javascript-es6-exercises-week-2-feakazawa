5ead8a070aeb9ab07fa0006c2736d5959a0a11bc
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-31 22:45:01 -0700

    exercise 18 done

--- ex18.js
+++ ex18.js
@@ -1,7 +1,10 @@
-/*One classic method for composing secret messages is called a square code. In this activity, we will be creating our own Square Code algorithm.
+/*One classic method for composing secret messages is called a square code. In this activity, we will be creating our 
+own Square Code algorithm.
 
 Square Code
-In Square Code, the spaces are removed from the english text and the characters are written into a square (or rectangle). For example, the sentence "If man was meant to stay on the ground god would have given us roots" is 54 characters long, once the spaces are removed, so it is written into a rectangle with 7 rows and 8 columns.
+In Square Code, the spaces are removed from the english text and the characters are written into a square 
+(or rectangle). For example, the sentence "If man was meant to stay on the ground god would have given us roots" 
+is 54 characters long, once the spaces are removed, so it is written into a rectangle with 7 rows and 8 columns.
 
     ifmanwas
     meanttos
@@ -10,7 +13,9 @@ In Square Code, the spaces are removed from the english text and the characters 
     dwouldha
     vegivenu
     sroots
-The square root of 54 (Math.sqrt(54)) is 7.3484692283495345. If we round this number up (Math.ceil(Math.sqrt(54))), we get 8. If we use that for the number of characters on each line (the number of columns), then our text will be close to a square shape.
+The square root of 54 (Math.sqrt(54)) is 7.3484692283495345. If we round this number up (Math.ceil(Math.sqrt(54))), 
+we get 8. If we use that for the number of characters on each line (the number of columns), then our text will be close 
+to a square shape.
 
 The message is then coded by reading down the columns going left to right. For example, the message above is coded as:
 
@@ -22,17 +27,52 @@ Instruction
 Create a function named squareCode that will receive a message, and return the secret square code version of the message.
 */
 
+const nthChar = (word, idx) => word[idx];
+
 const squareCode = function (message) {
-  // Put your solution here
+  let newMsg = message.split(" ").join("");
+  let colNumb = Math.ceil(Math.sqrt(newMsg.length));
+  let splitMsg = newMsg.split("");
+  let line = "";
+  let rectangleWords = [];
+
+  //build line rectangle
+  for (char of splitMsg) {
+    line += char;
+
+    //create every line with colnumb length
+    if (line.length % colNumb === 0) {
+      rectangleWords.push(line);
+      line = "";
+    }
+  }
+  rectangleWords.push(line); //add remaining characteres
+
+  const finalCode = generateOutput(rectangleWords, colNumb);
+  return finalCode;
 };
+
+function generateOutput(arr, limit) {
+  let output = [];
+  let finalOutput = "";
+
+  for (let i = 0; i < limit; i++) {
+    arr.forEach((word) => {
+      output.push(nthChar(word, i));
+    });
+    output.push(" ");
+  }
+  finalOutput += output.join("");
+  return finalOutput;
+}
 
 console.log(squareCode("chill out")); // clu hlt io
 console.log(squareCode("feed the dog")); // fto ehg ee dd
 console.log(squareCode("have a nice day")); // hae and via ecy
 console.log(
   squareCode(
-    "if man was meant to stay on the ground god would have given us roots"
-  )
+    "if man was meant to stay on the ground god would have given us roots",
+  ),
 ); // imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
 
 module.exports = squareCode;
